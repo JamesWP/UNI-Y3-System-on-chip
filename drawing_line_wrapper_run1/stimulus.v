3ac2197efcc841f1506d74a705e9fe35729644cf
James Peach <james.peach@student.manchester.ac.uk>
2015-10-20 12:53:41 +0100

    changed tests

--- drawing_line_wrapper_run1/stimulus.v
+++ drawing_line_wrapper_run1/stimulus.v
@@ -29,7 +29,17 @@ end
 always @(posedge error)
 begin
   $display("[%4d] Error occured", clkNo);
-  #100 $stop;
+  #100 $finish;
+end
+
+always @(posedge clk)
+begin
+  if(ack)
+  begin
+    $display("[%4d] Test Line Start (%3d,%3d)->(%3d,%3d)",clkNo, 
+      r0, r1, r2, r3, r6);
+    @(negedge ack);
+  end
 end
 
 always @(posedge clk)
@@ -39,8 +49,18 @@ case (clkNo)
   100: test1;
   
   200: test2;
+  
+  300: test3;
 
-  300: $stop;
+  400: test_line(010,010,200,010,8'b111_111_11);
+
+  500: test_line(010,010,100,020,8'b111_111_11);
+
+  600: test_line(010,010,000,000,8'b111_111_11);
+
+  700: test_line(000,000,639,479,8'b111_111_11);
+ 
+  10000: $finish;
 endcase
 
 // Tasks
@@ -90,6 +110,110 @@ begin
 end
 endtask
 
+task test_line;
+input integer sx;
+input integer sy;
+input integer ex;
+input integer ey;
+input integer color;
+begin
+
+  req = 0;
+  r0 = 0;
+  r1 = 0;
+  r2 = 0;
+  r3 = 0;
+  r4 = 0;
+  r5 = 0;
+  r6 = 0;
+  r7 = 0;
+  
+  de_ack = 0;
+
+  #30  
+
+  $display("[%4d] Parameters Set",clkNo);
+
+  // from (r0,r1)
+  r0 = sx;
+  r1 = sy;
+  // to (r2,r3)
+  r2 = ex;
+  r3 = ey;
+  // in color r6
+  //      RRR GGG BB
+  r6 = color;
+
+  #1 req = 1;
+
+  $display("[%4d] Waiting for ack",clkNo);
+  #1 @(posedge ack)
+  $display("[%4d] Got ack",clkNo);  
+
+  @(posedge clk)
+  #1 req = 0;
+  
+  // wait for module to not be busy
+  @(negedge busy)
+  
+  $display("[%4d] Finished plotting",clkNo);
+end 
+endtask
+
+
+task test3;
+begin
+  $display("[%4d] Test 3 Start",clkNo);
+
+  req = 0;
+  r0 = 0;
+  r1 = 0;
+  r2 = 0;
+  r3 = 0;
+  r4 = 0;
+  r5 = 0;
+  r6 = 0;
+  r7 = 0;
+  
+  de_ack = 0;
+
+  #30  
+
+  $display("[%4d] Parameters Set",clkNo);
+
+  // from (r0,r1)
+  r0 = 00;
+  r1 = 00;
+  // to (r2,r3)
+  r2 = 10;
+  r3 = 10;
+  // in color r6
+  //      RRR GGG BB
+  r6 = 8'b100_10_10;
+
+  #1 req = 1;
+
+  $display("[%4d] Waiting for ack",clkNo);
+  #1 @(posedge ack)
+  $display("[%4d] Got ack",clkNo);  
+
+  @(posedge clk)
+  #1 req = 0;
+  
+  @(posedge clk)
+
+  r1 = 14;
+  r2 = 14; 
+  r6 = 0;
+ 
+  $display("[%4d] Changed data",clkNo);
+  
+  // wait for module to not be busy
+  @(negedge busy)
+  
+  $display("[%4d] Finished plotting",clkNo);
+end 
+endtask
 
 
 task test2;
