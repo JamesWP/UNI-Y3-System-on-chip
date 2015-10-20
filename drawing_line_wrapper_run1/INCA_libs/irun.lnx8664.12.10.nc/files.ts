f1373f93aa4837a3fa215ecf7e539769c89549cd
James Peach <james.peach@student.manchester.ac.uk>
2015-10-20 12:53:56 +0100

    t :w

--- drawing_line_wrapper_run1/INCA_libs/irun.lnx8664.12.10.nc/files.ts
+++ drawing_line_wrapper_run1/INCA_libs/irun.lnx8664.12.10.nc/files.ts
@@ -1,5 +1,5 @@
 1444147926 /home/mbax3jp2/Cadence/COMP32211/drawing_line_wrapper_run1/cds.lib
 1444147926 /home/mbax3jp2/Cadence/COMP32211/drawing_line_wrapper_run1/hdl.var
-1444751805 /home/mbax3jp2/Cadence/COMP32211/drawing_line_wrapper_run1/testfixture.template
-1444751805 /home/mbax3jp2/Cadence/COMP32211/drawing_line_wrapper_run1/ihnl/cds0/netlist
+1445341862 /home/mbax3jp2/Cadence/COMP32211/drawing_line_wrapper_run1/testfixture.template
+1445341862 /home/mbax3jp2/Cadence/COMP32211/drawing_line_wrapper_run1/ihnl/cds0/netlist
 1132137284 /cadtools5/ise8.1i/verilog/src/glbl.v
