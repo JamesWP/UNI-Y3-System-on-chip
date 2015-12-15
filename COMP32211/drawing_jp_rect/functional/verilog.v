5c855f4dc340fb88b86c9178ff686049c2ef71ad
James Peach <james.peach@student.manchester.ac.uk>
2015-12-15 16:34:23 +0000

    added comments

--- COMP32211/drawing_jp_rect/functional/verilog.v
+++ COMP32211/drawing_jp_rect/functional/verilog.v
@@ -1,6 +1,26 @@
-// Verilog HDL for "COMP32211", "drawing_dummy" "functional"
-// This is an inactive cell which takes the place of a drawing function.
-// It 'ties off' outputs tidily.
+// Verilog HDL for "COMP32211", "drawing_jp_rect" "functional"
+
+/*
+ *
+ * Verilog rectangle drawing module
+ *
+ * Module will draw rectangles on the screen with the given colors and pattern
+ *
+ * The modules inputs are as follows
+ *
+ * wire_name | bits | description      | example
+ * ----------|------|------------------|--------
+ * r0        | 16   | start x pos      | 16'd10
+ * r1        | 16   | start y pos      | 16'd10
+ * r2[15:8]  | 8    | width            | 16'd200
+ * r2[ 7:0]  | 8    | height           | 16'd200
+ * r3[15:8]  | 8    | colour 1         |  8'b111_111_11
+ * r3[ 7:0]  | 8    | colour 2         |  8'b101_100_01
+ * r4        | 16   | pattern rows 1-2 | 16'b11111111_11111111
+ * r5        | 16   | pattern rows 3-4 | 16'b11111111_11111111
+ * r6        | 16   | pattern rows 5-6 | 16'b11111111_11111111
+ * r7        | 16   | pattern rows 7-8 | 16'b11111111_11111111
+ * */
 
 `define TPD 2
 
@@ -26,43 +46,66 @@ module drawing_jp_rect( input  wire        clk,
 
 
 // state enumeration
-`define STATE_START 0
-`define STATE_ACK 1
-`define STATE_CALCULATE 2
-`define STATE_DRAW 3
-`define STATE_DONE 4
+`define STATE_START 0      // state to await input request
+`define STATE_ACK 1        // state to send ACK signal and latch params
+`define STATE_CALCULATE 2  // state to calculate the next column params
+`define STATE_DRAW 3       // state to draw the curent pixel block
+`define STATE_DONE 4       // state to signal finished drawing
 
 // fsm state
 reg  [2:0]  state;
-initial state = `STATE_START;
-// internal transition signals
-wire        cols_to_draw;  // are there more columns to draw after this
-wire        last_col;
-wire        rows_to_draw;
+initial state = `STATE_START; // added to fix bug of module starting on FPGA
+                              // in incorrect state
+
+// internal transition signals, used by the fsm to decide on next state
+wire        cols_to_draw;  // are there more columns to draw after this?
+wire        last_col;      // is this the last column in the rectangle?
+wire        rows_to_draw;  // is there any more rows to draw after this one?
+
 // request params
-reg  [15:0] startx;
-wire [15:0] startxWord; // word alligned value of startx
-wire [15:0] nextStartx; // next value of startx (after this col is done) 
-wire [ 2:0] pxDone; // number of pixels complete in this col (0,1,2,3,4)
-reg  [15:0] starty;
-reg  [ 7:0] width;
-wire [15:0] nextWidth; // the next value of width (after this col)
-reg  [ 7:0] height;
-// coldraw params
-reg  [15:0] remHeight; //remaining height to draw
-wire [15:0] nextRemHeight;
-wire [15:0] colx; // column to draw (word alligned)
-reg  [ 3:0] colmask;
-wire [15:0] coloffset;
-wire [15:0] curY;
-
+reg  [15:0] startx;        // stores the curent start x position value
+reg  [15:0] starty;        // stores the starting y position value
+reg  [ 7:0] width;         // stores the remaining width left to
+                           // draw (from startx)
+reg  [ 7:0] height;        // stores the height of the rectangle
 reg  [ 7:0] colourPalet [1:0];
+                           // stores the two colours to be drawn
+reg  [15:0] patternRegBank [3:0];
+                           // stores the 8x8 pattern in 4 "reg's"
+                           // these are arranged as follows
+                           //0[----|----]
+                           // [----|----]
+                           //1[----|----]
+                           // [----|----]
+                           //2[----|----]
+                           // [----|----]
+                           //3[----|----]
+                           // [----|----]
+                           //
+                           // where each row above is supplied in a seperate
+                           // reg
 
-reg  [15:0] patternRegBank [3:0]; // all the patterns
+// calculated values (assigns)
+wire [15:0] startxWord;    // word alligned value of startx
+wire [15:0] nextStartx;    // next value of startx (after this col is done) 
+wire [ 2:0] pxDone;        // number of pixels complete in this col (1,2,3,4)
+wire [15:0] nextWidth;     // the next value of width (after this col)
+
+// coldraw params
+reg  [15:0] remHeight;     // remaining height to draw in this column
+
+// calculated values (assigns)
+wire [15:0] nextRemHeight; // the next value of remHeight
+wire [15:0] colx;          // column to draw (word alligned)
+reg  [ 3:0] colmask;       // this columns mask
+wire [15:0] coloffset;     // the offset into this column of the first drawn px
+wire [15:0] curY;          // the current Y value of this row
+
+
 wire [ 1:0] patternRegSel; // the selected pattern from bank
-wire [15:0] patternReg; // the value of the selected pattern bank
-wire [ 7:0] row; // the row from the selected pattern bank
-wire [ 3:0] pattern; // the column from the row
+wire [15:0] patternReg;    // the value of the selected pattern bank
+wire [ 7:0] row;           // the row from the selected pattern bank
+wire [ 3:0] pattern;       // the column from the row
 
 // STATE TRANSITION FOR FSM
 always @ (posedge clk)
@@ -133,23 +176,22 @@ begin
   endcase
 end
 
-
+// assign outputs
 assign busy = (state==`STATE_START||state==`STATE_DONE)? 0:1;
 assign ack = (state==`STATE_ACK)? 1:0;
+// assign signals for fsm
 assign cols_to_draw = (width>0)? 1:0;
 assign last_col = (nextWidth>0)? 1:0;
+assign rows_to_draw = nextRemHeight>0;
+// assign helper signals
 assign startxWord = startx&((0-1)<<2);
 assign pxDone = (width>3)? (4) - (startx & 2'h3):width;
 assign nextStartx = startxWord + (1<<2);
 assign nextWidth = width - pxDone;
 assign nextRemHeight = remHeight - 1;
-assign rows_to_draw = nextRemHeight>0;
-
 assign curY = starty+nextRemHeight;
-
 assign colx = startxWord;
 assign coloffset = startx - startxWord;
-
 // color assignments
 assign patternRegSel = curY[2:1];
 assign patternReg = patternRegBank[patternRegSel];
@@ -157,7 +199,7 @@ assign row = curY[0]? patternReg[15:8]:patternReg[7:0];
 assign pattern = startxWord[2]? row[3:0]:row[7:4];
 
 
-//TODO: assign values
+// assign output pixel draw
 assign de_req = (state==`STATE_DRAW)? 1:0;
 assign de_addr =  ( (curY<<5) + (curY<<7) + (startxWord>>2) );
 assign de_nbyte = ~colmask;
@@ -170,7 +212,7 @@ assign de_w_data = {
 
 
 // calculate colmask
-
+// this is a selection of which pixels to draw
 always @ (coloffset,pxDone)
 begin
   case(pxDone)
